0af66d6ed5057e6ff96f9d1e778f71b2221a09aa
Andriy-Lyt <howdy_eh@icloud.com>
2021-06-26 23:32:19 +0000

    JS assignment done

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let pizzaToppings = ['tomato', 'sauce', 'cheese',  'pepperoni'];
+
+console.log(pizzaToppings);
+
+for (let index = 0; index < pizzaToppings.length; index++) {
+  console.log(pizzaToppings[index]);  
+}
